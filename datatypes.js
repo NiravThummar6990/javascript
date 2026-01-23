5d55dc01fd9de8e461016a1afdd283e5d57ffa8d
NiravThummar6990 <niravthummar6990@gmail.com>
2026-01-23 23:40:05 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert("Hello")  // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Nirav")
+
+let name ="Nirav"
+let age = 18
+let inLoggin = true
+
+// number => 2 to power 53
+// bigint (bv j moto aakdo)
+// string => ""
+// Boolean => true, false
+// null => standalone value
+// undefined => (value asign nai thay)
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined)
+    ;
---
+++ variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "nirav12@gmail.com"
+var accountPassword = "12333"
+accountCity ="amd"
+let accounto="null"
+let accontn;
+accountEmail="nnn@gmail.com"
+accountPassword="1221222222"
+accountCity="bengluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accontn,accounto])
