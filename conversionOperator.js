8bb8b3db58847007c8ca45a4e7cf78c97556acb2
NiravThummar6990 <niravthummar6990@gmail.com>
2026-01-23 23:56:34 +0530

    Conversion of operators

---
+++ conversionOperator.js
@@ -0,0 +1,22 @@
+let score ="33hgfxdfgh"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof (valueInNumber));
+console.log (valueInNumber);
+
+// "33" => 33
+// "33feff" => NaN
+// true => 1; false =>2
+
+let isboolean= 1
+console.log(typeof isboolean)
+let inbul=Boolean(isboolean)
+console.log(inbul);
+
+ //(boolean)
+// 1 => true
+// "nirav" => true
+// "" => false
